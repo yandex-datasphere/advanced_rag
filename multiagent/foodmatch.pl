2aaa1b1906a1ad4d174f6e28a2559d77299656e8
Dmitri Soshnikov <dmitri@soshnikov.com>
2024-09-09 22:17:09 +0300

    Add multiagent demo

---
+++ multiagent/foodmatch.pl
@@ -0,0 +1,446 @@
+# MealCourse
+# BlandFishCourse
+foodmatch(food_BlandFish,hasBody,medium).
+foodmatch(food_BlandFish,hasColor,white).
+foodmatch(food_BlandFish,hasSugar,dry).
+foodmatch(food_BlandFish,hasFlavor,delicate).
+# CheeseNutsDessertCourse
+foodmatch(food_CheeseNutsDessert,hasSugar,sweet).
+foodmatch(food_CheeseNutsDessert,hasBody,full).
+foodmatch(food_CheeseNutsDessert,hasColor,red).
+foodmatch(food_CheeseNutsDessert,hasFlavor,strong).
+# DarkMeatFowlCourse
+foodmatch(food_DarkMeatFowl,hasBody,light).
+foodmatch(food_DarkMeatFowl,hasSugar,dry).
+foodmatch(food_DarkMeatFowl,hasColor,red).
+foodmatch(food_DarkMeatFowl,hasFlavor,delicate).
+# DessertCourse
+foodmatch(food_Dessert,hasSugar,sweet).
+foodmatch(food_Dessert,hasBody,full).
+foodmatch(food_Dessert,hasFlavor,strong).
+foodmatch(food_Dessert,hasColor,_).
+# FishCourse
+foodmatch(food_Fish,hasBody,medium).
+foodmatch(food_Fish,hasColor,white).
+foodmatch(food_Fish,hasSugar,dry).
+foodmatch(food_Fish,hasFlavor,_).
+# FruitCourse
+foodmatch(food_Fruit,hasBody,medium).
+foodmatch(food_Fruit,hasColor,white).
+foodmatch(food_Fruit,hasSugar,_).
+foodmatch(food_Fruit,hasFlavor,_).
+# LightMeatFowlCourse
+foodmatch(food_LightMeatFowl,hasBody,medium).
+foodmatch(food_LightMeatFowl,hasFlavor,moderate).
+foodmatch(food_LightMeatFowl,hasColor,white).
+foodmatch(food_LightMeatFowl,hasSugar,dry).
+# NonBlandFishCourse
+foodmatch(food_NonBlandFish,hasBody,medium).
+foodmatch(food_NonBlandFish,hasFlavor,moderate).
+foodmatch(food_NonBlandFish,hasSugar,dry).
+foodmatch(food_NonBlandFish,hasColor,white).
+# NonOysterShellfishCourse
+foodmatch(food_NonOysterShellfish,hasBody,full).
+foodmatch(food_NonOysterShellfish,hasFlavor,moderate).
+foodmatch(food_NonOysterShellfish,hasFlavor,strong).
+foodmatch(food_NonOysterShellfish,hasColor,white).
+foodmatch(food_NonOysterShellfish,hasSugar,dry).
+# NonRedMeatCourse
+foodmatch(food_NonRedMeat,hasBody,medium).
+foodmatch(food_NonRedMeat,hasColor,white).
+foodmatch(food_NonRedMeat,hasSugar,dry).
+foodmatch(food_NonRedMeat,hasFlavor,strong).
+# NonSpicyRedMeatCourse
+foodmatch(food_NonSpicyRedMeat,hasBody,medium).
+foodmatch(food_NonSpicyRedMeat,hasFlavor,moderate).
+foodmatch(food_NonSpicyRedMeat,hasFlavor,strong).
+foodmatch(food_NonSpicyRedMeat,hasColor,red).
+foodmatch(food_NonSpicyRedMeat,hasSugar,dry).
+# NonSweetFruitCourse
+foodmatch(food_NonSweetFruit,hasBody,medium).
+foodmatch(food_NonSweetFruit,hasColor,white).
+foodmatch(food_NonSweetFruit,hasSugar,offdry).
+foodmatch(food_NonSweetFruit,hasFlavor,delicate).
+# OtherTomatoBasedFoodCourse
+foodmatch(food_OtherTomatoBasedFood,hasBody,medium).
+foodmatch(food_OtherTomatoBasedFood,hasFlavor,moderate).
+foodmatch(food_OtherTomatoBasedFood,hasColor,red).
+foodmatch(food_OtherTomatoBasedFood,hasSugar,dry).
+# OysterShellfishCourse
+foodmatch(food_OysterShellfish,hasSugar,sweet).
+foodmatch(food_OysterShellfish,hasBody,full).
+foodmatch(food_OysterShellfish,hasFlavor,moderate).
+foodmatch(food_OysterShellfish,hasFlavor,strong).
+foodmatch(food_OysterShellfish,hasColor,white).
+# PastaWithHeavyCreamCourse
+foodmatch(food_PastaWithHeavyCream,hasBody,medium).
+foodmatch(food_PastaWithHeavyCream,hasFlavor,moderate).
+foodmatch(food_PastaWithHeavyCream,hasColor,white).
+foodmatch(food_PastaWithHeavyCream,hasSugar,dry).
+# PastaWithLightCreamCourse
+foodmatch(food_PastaWithLightCream,hasBody,light).
+foodmatch(food_PastaWithLightCream,hasColor,white).
+foodmatch(food_PastaWithLightCream,hasSugar,dry).
+foodmatch(food_PastaWithLightCream,hasFlavor,delicate).
+# PastaWithNonSpicyRedSauceCourse
+foodmatch(food_PastaWithNonSpicyRedSauce,hasBody,medium).
+foodmatch(food_PastaWithNonSpicyRedSauce,hasFlavor,moderate).
+foodmatch(food_PastaWithNonSpicyRedSauce,hasColor,red).
+foodmatch(food_PastaWithNonSpicyRedSauce,hasSugar,dry).
+# PastaWithSpicyRedSauceCourse
+foodmatch(food_PastaWithSpicyRedSauce,hasBody,full).
+foodmatch(food_PastaWithSpicyRedSauce,hasColor,red).
+foodmatch(food_PastaWithSpicyRedSauce,hasSugar,dry).
+foodmatch(food_PastaWithSpicyRedSauce,hasFlavor,strong).
+# RedMeatCourse
+foodmatch(food_RedMeat,hasColor,red).
+foodmatch(food_RedMeat,hasSugar,_).
+foodmatch(food_RedMeat,hasFlavor,_).
+foodmatch(food_RedMeat,hasBody,_).
+# SeafoodCourse
+foodmatch(food_Seafood,hasColor,white).
+foodmatch(food_Seafood,hasSugar,_).
+foodmatch(food_Seafood,hasFlavor,_).
+foodmatch(food_Seafood,hasBody,_).
+# ShellfishCourse
+foodmatch(food_Shellfish,hasBody,full).
+foodmatch(food_Shellfish,hasFlavor,moderate).
+foodmatch(food_Shellfish,hasFlavor,strong).
+foodmatch(food_Shellfish,hasColor,white).
+foodmatch(food_Shellfish,hasSugar,_).
+# SpicyRedMeatCourse
+foodmatch(food_SpicyRedMeat,hasBody,full).
+foodmatch(food_SpicyRedMeat,hasFlavor,moderate).
+foodmatch(food_SpicyRedMeat,hasColor,red).
+foodmatch(food_SpicyRedMeat,hasSugar,dry).
+# SweetDessertCourse
+foodmatch(food_SweetDessert,hasSugar,sweet).
+foodmatch(food_SweetDessert,hasBody,full).
+foodmatch(food_SweetDessert,hasColor,white).
+foodmatch(food_SweetDessert,hasFlavor,strong).
+# SweetFruitCourse
+foodmatch(food_SweetFruit,hasSugar,sweet).
+foodmatch(food_SweetFruit,hasBody,medium).
+foodmatch(food_SweetFruit,hasFlavor,moderate).
+foodmatch(food_SweetFruit,hasColor,white).
+# Port
+wine(wine_Port,hasSugar,sweet).
+wine(wine_Port,hasBody,full).
+wine(wine_Port,hasFlavor,strong).
+wine(wine_Port,hasColor,red).
+# Semillon
+wine(wine_Semillon,hasColor,white).
+wine(wine_Semillon,hasBody,full).
+wine(wine_Semillon,hasBody,medium).
+wine(wine_Semillon,hasSugar,_).
+wine(wine_Semillon,hasFlavor,_).
+# AmericanWine
+# TableWine
+wine(wine_TableWine,hasSugar,dry).
+wine(wine_TableWine,hasFlavor,_).
+wine(wine_TableWine,hasBody,_).
+wine(wine_TableWine,hasColor,_).
+# Beaujolais
+wine(wine_Beaujolais,hasColor,red).
+wine(wine_Beaujolais,hasSugar,dry).
+wine(wine_Beaujolais,hasBody,light).
+wine(wine_Beaujolais,hasFlavor,delicate).
+# RedBurgundy
+wine(wine_RedBurgundy,hasColor,red).
+wine(wine_RedBurgundy,hasSugar,dry).
+wine(wine_RedBurgundy,hasBody,_).
+wine(wine_RedBurgundy,hasFlavor,_).
+# DessertWine
+wine(wine_DessertWine,hasSugar,offdry).
+wine(wine_DessertWine,hasSugar,sweet).
+wine(wine_DessertWine,hasFlavor,_).
+wine(wine_DessertWine,hasBody,_).
+wine(wine_DessertWine,hasColor,_).
+# Medoc
+wine(wine_Medoc,hasColor,red).
+wine(wine_Medoc,hasSugar,dry).
+wine(wine_Medoc,hasBody,_).
+wine(wine_Medoc,hasFlavor,_).
+# Bordeaux
+# Loire
+# CaliforniaWine
+# WhiteBurgundy
+wine(wine_WhiteBurgundy,hasColor,white).
+wine(wine_WhiteBurgundy,hasSugar,dry).
+wine(wine_WhiteBurgundy,hasSugar,offdry).
+wine(wine_WhiteBurgundy,hasFlavor,moderate).
+wine(wine_WhiteBurgundy,hasFlavor,strong).
+wine(wine_WhiteBurgundy,hasBody,full).
+wine(wine_WhiteBurgundy,hasBody,medium).
+# Anjou
+wine(wine_Anjou,hasSugar,offdry).
+wine(wine_Anjou,hasColor,rose).
+wine(wine_Anjou,hasBody,light).
+wine(wine_Anjou,hasFlavor,delicate).
+# PinotNoir
+wine(wine_PinotNoir,hasColor,red).
+wine(wine_PinotNoir,hasSugar,_).
+wine(wine_PinotNoir,hasFlavor,_).
+wine(wine_PinotNoir,hasBody,_).
+# GermanWine
+# CotesDOr
+wine(wine_CotesDOr,hasColor,red).
+wine(wine_CotesDOr,hasFlavor,moderate).
+wine(wine_CotesDOr,hasSugar,dry).
+wine(wine_CotesDOr,hasBody,_).
+# PetiteSyrah
+wine(wine_PetiteSyrah,hasColor,red).
+wine(wine_PetiteSyrah,hasFlavor,moderate).
+wine(wine_PetiteSyrah,hasFlavor,strong).
+wine(wine_PetiteSyrah,hasSugar,dry).
+wine(wine_PetiteSyrah,hasBody,full).
+wine(wine_PetiteSyrah,hasBody,medium).
+# FrenchWine
+# Meursault
+wine(wine_Meursault,hasColor,white).
+wine(wine_Meursault,hasSugar,dry).
+wine(wine_Meursault,hasSugar,offdry).
+wine(wine_Meursault,hasBody,full).
+wine(wine_Meursault,hasFlavor,moderate).
+wine(wine_Meursault,hasFlavor,strong).
+wine(wine_Meursault,hasBody,medium).
+# RedTableWine
+wine(wine_RedTableWine,hasColor,red).
+wine(wine_RedTableWine,hasSugar,dry).
+wine(wine_RedTableWine,hasBody,_).
+wine(wine_RedTableWine,hasFlavor,_).
+# StEmilion
+wine(wine_StEmilion,hasColor,red).
+wine(wine_StEmilion,hasFlavor,moderate).
+wine(wine_StEmilion,hasFlavor,strong).
+wine(wine_StEmilion,hasSugar,dry).
+wine(wine_StEmilion,hasBody,full).
+wine(wine_StEmilion,hasBody,medium).
+# WhiteBordeaux
+wine(wine_WhiteBordeaux,hasColor,white).
+wine(wine_WhiteBordeaux,hasBody,full).
+wine(wine_WhiteBordeaux,hasBody,medium).
+wine(wine_WhiteBordeaux,hasSugar,_).
+wine(wine_WhiteBordeaux,hasFlavor,_).
+# DryWine
+wine(wine_DryWine,hasSugar,dry).
+wine(wine_DryWine,hasFlavor,_).
+wine(wine_DryWine,hasBody,_).
+wine(wine_DryWine,hasColor,_).
+# ItalianWine
+# Muscadet
+wine(wine_Muscadet,hasColor,white).
+wine(wine_Muscadet,hasSugar,dry).
+wine(wine_Muscadet,hasSugar,offdry).
+wine(wine_Muscadet,hasBody,light).
+wine(wine_Muscadet,hasFlavor,delicate).
+# Margaux
+wine(wine_Margaux,hasColor,red).
+wine(wine_Margaux,hasBody,light).
+wine(wine_Margaux,hasBody,medium).
+wine(wine_Margaux,hasSugar,dry).
+wine(wine_Margaux,hasFlavor,delicate).
+wine(wine_Margaux,hasFlavor,moderate).
+# TexasWine
+# WhiteWine
+wine(wine_WhiteWine,hasColor,white).
+wine(wine_WhiteWine,hasSugar,_).
+wine(wine_WhiteWine,hasFlavor,_).
+wine(wine_WhiteWine,hasBody,_).
+# Riesling
+wine(wine_Riesling,hasColor,white).
+wine(wine_Riesling,hasSugar,_).
+wine(wine_Riesling,hasFlavor,_).
+wine(wine_Riesling,hasBody,_).
+# SemillonOrSauvignonBlanc
+wine(wine_SemillonOrSauvignonBlanc,hasColor,white).
+wine(wine_SemillonOrSauvignonBlanc,hasBody,full).
+wine(wine_SemillonOrSauvignonBlanc,hasBody,medium).
+wine(wine_SemillonOrSauvignonBlanc,hasSugar,_).
+wine(wine_SemillonOrSauvignonBlanc,hasFlavor,_).
+# Pauillac
+wine(wine_Pauillac,hasBody,full).
+wine(wine_Pauillac,hasColor,red).
+wine(wine_Pauillac,hasFlavor,moderate).
+wine(wine_Pauillac,hasFlavor,strong).
+wine(wine_Pauillac,hasSugar,dry).
+wine(wine_Pauillac,hasBody,medium).
+# RedBordeaux
+wine(wine_RedBordeaux,hasColor,red).
+wine(wine_RedBordeaux,hasSugar,_).
+wine(wine_RedBordeaux,hasFlavor,_).
+wine(wine_RedBordeaux,hasBody,_).
+# Meritage
+wine(wine_Meritage,hasColor,red).
+wine(wine_Meritage,hasSugar,_).
+wine(wine_Meritage,hasFlavor,_).
+wine(wine_Meritage,hasBody,_).
+# Wine
+# PinotBlanc
+wine(wine_PinotBlanc,hasColor,white).
+wine(wine_PinotBlanc,hasSugar,_).
+wine(wine_PinotBlanc,hasFlavor,_).
+wine(wine_PinotBlanc,hasBody,_).
+# AlsatianWine
+# Sancerre
+wine(wine_Sancerre,hasColor,white).
+wine(wine_Sancerre,hasSugar,dry).
+wine(wine_Sancerre,hasSugar,offdry).
+wine(wine_Sancerre,hasBody,medium).
+wine(wine_Sancerre,hasBody,full).
+wine(wine_Sancerre,hasFlavor,delicate).
+# Burgundy
+wine(wine_Burgundy,hasSugar,dry).
+wine(wine_Burgundy,hasFlavor,_).
+wine(wine_Burgundy,hasBody,_).
+wine(wine_Burgundy,hasColor,_).
+# SweetWine
+wine(wine_SweetWine,hasSugar,sweet).
+wine(wine_SweetWine,hasFlavor,_).
+wine(wine_SweetWine,hasBody,_).
+wine(wine_SweetWine,hasColor,_).
+# FullBodiedWine
+wine(wine_FullBodiedWine,hasBody,full).
+wine(wine_FullBodiedWine,hasSugar,_).
+wine(wine_FullBodiedWine,hasFlavor,_).
+wine(wine_FullBodiedWine,hasColor,_).
+# DryRiesling
+wine(wine_DryRiesling,hasColor,white).
+wine(wine_DryRiesling,hasSugar,dry).
+wine(wine_DryRiesling,hasSugar,offdry).
+wine(wine_DryRiesling,hasBody,light).
+wine(wine_DryRiesling,hasBody,medium).
+wine(wine_DryRiesling,hasFlavor,delicate).
+# CheninBlanc
+wine(wine_CheninBlanc,hasColor,white).
+wine(wine_CheninBlanc,hasSugar,dry).
+wine(wine_CheninBlanc,hasSugar,offdry).
+wine(wine_CheninBlanc,hasFlavor,moderate).
+wine(wine_CheninBlanc,hasBody,full).
+wine(wine_CheninBlanc,hasBody,medium).
+# SweetRiesling
+wine(wine_SweetRiesling,hasColor,white).
+wine(wine_SweetRiesling,hasSugar,sweet).
+wine(wine_SweetRiesling,hasBody,full).
+wine(wine_SweetRiesling,hasFlavor,moderate).
+wine(wine_SweetRiesling,hasFlavor,strong).
+wine(wine_SweetRiesling,hasSugar,offdry).
+# Wine
+# Chardonnay
+wine(wine_Chardonnay,hasColor,white).
+wine(wine_Chardonnay,hasFlavor,moderate).
+wine(wine_Chardonnay,hasFlavor,strong).
+wine(wine_Chardonnay,hasBody,full).
+wine(wine_Chardonnay,hasBody,medium).
+wine(wine_Chardonnay,hasSugar,_).
+# IceWine
+wine(wine_IceWine,hasColor,white).
+wine(wine_IceWine,hasSugar,sweet).
+wine(wine_IceWine,hasFlavor,moderate).
+wine(wine_IceWine,hasFlavor,strong).
+wine(wine_IceWine,hasSugar,offdry).
+wine(wine_IceWine,hasBody,full).
+wine(wine_IceWine,hasBody,medium).
+# SauvignonBlanc
+wine(wine_SauvignonBlanc,hasColor,white).
+wine(wine_SauvignonBlanc,hasBody,full).
+wine(wine_SauvignonBlanc,hasBody,medium).
+wine(wine_SauvignonBlanc,hasSugar,_).
+wine(wine_SauvignonBlanc,hasFlavor,_).
+# RedWine
+wine(wine_RedWine,hasColor,red).
+wine(wine_RedWine,hasSugar,_).
+wine(wine_RedWine,hasFlavor,_).
+wine(wine_RedWine,hasBody,_).
+# Gamay
+# Chianti
+wine(wine_Chianti,hasColor,red).
+wine(wine_Chianti,hasFlavor,moderate).
+wine(wine_Chianti,hasBody,light).
+wine(wine_Chianti,hasBody,medium).
+wine(wine_Chianti,hasSugar,dry).
+# DryRedWine
+wine(wine_DryRedWine,hasColor,red).
+wine(wine_DryRedWine,hasSugar,dry).
+wine(wine_DryRedWine,hasBody,_).
+wine(wine_DryRedWine,hasFlavor,_).
+# EarlyHarvest
+wine(wine_EarlyHarvest,hasSugar,dry).
+wine(wine_EarlyHarvest,hasSugar,offdry).
+wine(wine_EarlyHarvest,hasFlavor,_).
+wine(wine_EarlyHarvest,hasBody,_).
+wine(wine_EarlyHarvest,hasColor,_).
+# Merlot
+wine(wine_Merlot,hasColor,red).
+wine(wine_Merlot,hasBody,light).
+wine(wine_Merlot,hasBody,medium).
+wine(wine_Merlot,hasSugar,dry).
+wine(wine_Merlot,hasFlavor,delicate).
+wine(wine_Merlot,hasFlavor,moderate).
+# LateHarvest
+wine(wine_LateHarvest,hasSugar,sweet).
+wine(wine_LateHarvest,hasFlavor,moderate).
+wine(wine_LateHarvest,hasFlavor,strong).
+wine(wine_LateHarvest,hasBody,_).
+wine(wine_LateHarvest,hasColor,_).
+# WhiteTableWine
+wine(wine_WhiteTableWine,hasColor,white).
+wine(wine_WhiteTableWine,hasSugar,dry).
+wine(wine_WhiteTableWine,hasSugar,offdry).
+wine(wine_WhiteTableWine,hasBody,_).
+wine(wine_WhiteTableWine,hasFlavor,_).
+# WhiteLoire
+wine(wine_WhiteLoire,hasColor,white).
+wine(wine_WhiteLoire,hasSugar,_).
+wine(wine_WhiteLoire,hasFlavor,_).
+wine(wine_WhiteLoire,hasBody,_).
+# DryWhiteWine
+wine(wine_DryWhiteWine,hasColor,white).
+wine(wine_DryWhiteWine,hasSugar,dry).
+wine(wine_DryWhiteWine,hasSugar,offdry).
+wine(wine_DryWhiteWine,hasBody,_).
+wine(wine_DryWhiteWine,hasFlavor,_).
+# CabernetSauvignon
+wine(wine_CabernetSauvignon,hasColor,red).
+wine(wine_CabernetSauvignon,hasFlavor,moderate).
+wine(wine_CabernetSauvignon,hasFlavor,strong).
+wine(wine_CabernetSauvignon,hasSugar,dry).
+wine(wine_CabernetSauvignon,hasBody,full).
+wine(wine_CabernetSauvignon,hasBody,medium).
+# WhiteNonSweetWine
+wine(wine_WhiteNonSweetWine,hasColor,white).
+wine(wine_WhiteNonSweetWine,hasSugar,dry).
+wine(wine_WhiteNonSweetWine,hasSugar,offdry).
+wine(wine_WhiteNonSweetWine,hasBody,_).
+wine(wine_WhiteNonSweetWine,hasFlavor,_).
+# Tours
+wine(wine_Tours,hasColor,white).
+wine(wine_Tours,hasSugar,dry).
+wine(wine_Tours,hasSugar,offdry).
+wine(wine_Tours,hasFlavor,moderate).
+wine(wine_Tours,hasBody,full).
+wine(wine_Tours,hasBody,medium).
+# CabernetFranc
+wine(wine_CabernetFranc,hasBody,medium).
+wine(wine_CabernetFranc,hasColor,red).
+wine(wine_CabernetFranc,hasFlavor,moderate).
+wine(wine_CabernetFranc,hasSugar,dry).
+# Zinfandel
+wine(wine_Zinfandel,hasColor,red).
+wine(wine_Zinfandel,hasFlavor,moderate).
+wine(wine_Zinfandel,hasFlavor,strong).
+wine(wine_Zinfandel,hasSugar,dry).
+wine(wine_Zinfandel,hasBody,full).
+wine(wine_Zinfandel,hasBody,medium).
+# RoseWine
+wine(wine_RoseWine,hasColor,rose).
+wine(wine_RoseWine,hasSugar,_).
+wine(wine_RoseWine,hasFlavor,_).
+wine(wine_RoseWine,hasBody,_).
+# Sauternes
+wine(wine_Sauternes,hasColor,white).
+wine(wine_Sauternes,hasSugar,sweet).
+wine(wine_Sauternes,hasBody,medium).
+wine(wine_Sauternes,hasFlavor,moderate).
+wine(wine_Sauternes,hasFlavor,strong).
+wine(wine_Sauternes,hasBody,full).
